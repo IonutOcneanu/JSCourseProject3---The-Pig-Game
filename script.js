82e73fe3975861c61f8b3dcc172f4ece34ba81b3
IonutOcneanu <75932801+IonutOcneanu@users.noreply.github.com>
2020-12-23 01:07:46 +0200

    Update script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+//In work
+
+let diceRoll = document.querySelector(".roll-dice");
+
+//On click, roll the dice
+const rollDice = function () {
+  const dice = Math.trunc(Math.random() * 5) + 1;
+  return dice;
+};
+
+diceRoll.addEventListener("click", rollDice);
+
+TODO: // Create a div for each dice face (with one span for each dot) and display it based on the roll
