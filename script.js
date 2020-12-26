d430e9056a883703226caa58752e8bae3ccab6f2
IonutOcneanu <75932801+IonutOcneanu@users.noreply.github.com>
2020-12-26 16:59:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -25,14 +25,27 @@ const showDiceFaces = document.querySelectorAll(".dice");
 const showDice = document.querySelector(".dice-box");
 const newGame = document.querySelector(".new-game");
 const totalScore = document.querySelectorAll(".round-score");
-const currentScore = document.querySelectorAll(".score");
+const currentScores = document.querySelectorAll(".score");
 const holdBtn = document.querySelector(".hold");
 
-//It's called on each roll button click
-let rolledNumber;
+//Resets the dice on second and onward rollBtn clicks (hides faces if visible)
+let isClick = 0;
+const hideDice = function () {
+  if (isClick !== 0) {
+    for (let i = 1; i <= showDiceFaces.length; i++) {
+      if (!showDiceFaces[i].classList.contains("hidden")) {
+        showDiceFaces[i].classList.add("hidden");
+        break;
+      }
+    }
+  }
+};
 
+//It's called on each rollBtn click
+let rolledNumber;
 function rollDice() {
   rolledNumber = Math.trunc(Math.random() * 6) + 1;
+  console.log(rolledNumber);
   return rolledNumber;
 }
 
@@ -40,24 +53,53 @@ function rollDice() {
 const displayDice = function () {
   rollDice();
   showDice.classList.remove("dice-noshow");
-  console.log(rolledNumber);
   for (let i = 1; i <= showDiceFaces.length; i++) {
     if (rolledNumber === i) {
-      console.log(showDiceFaces[i]);
       showDiceFaces[i].classList.remove("hidden");
+      addScore();
+      isClick = 1;
+      break;
     }
   }
-  return rolledNumber;
 };
-
+rollBtn.addEventListener("click", hideDice); //only runs if isClick === 1;
 rollBtn.addEventListener("click", displayDice);
+
+//Current score & player turn update
+const activePlayerOne = document.getElementById("one");
+const activePlayerTwo = document.getElementById("two");
+
+function addScore() {
+  //Player 1
+  if (activePlayerOne.style.opacity === "1" && rolledNumber !== 1) {
+    currentScores[0].textContent =
+      parseInt(currentScores[0].textContent, 10) + rolledNumber;
+  } else if (activePlayerOne.style.opacity === "1" && rolledNumber === 1) {
+    currentScores[0].textContent = 0;
+    activePlayerOne.style.opacity = "0.4";
+    activePlayerTwo.style.opacity = "1";
+  }
+  //Player 2
+  else if (activePlayerTwo.style.opacity === "1" && rolledNumber !== 1) {
+    currentScores[1].textContent =
+      parseInt(currentScores[1].textContent, 10) + rolledNumber;
+  } else if (activePlayerTwo.style.opacity === "1" && rolledNumber === 1) {
+    currentScores[1].textContent = 0;
+    activePlayerOne.style.opacity = "1";
+    activePlayerTwo.style.opacity = "0.4";
+  }
+}
 
 //Start a new game
 
 const resetGame = function () {
-  currentScore.textContent = "0";
-  totalScore.textContent = "0";
+  currentScores[0].textContent = "0";
+  currentScores[1].textContent = "0";
+  totalScore[0].textContent = "0";
+  totalScore[1].textContent = "0";
   showDice.classList.add("dice-noshow");
+  activePlayerOne.style.opacity = "1";
+  activePlayerTwo.style.opacity = "0.4";
 };
 
 newGame.addEventListener("click", resetGame);
